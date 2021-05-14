24356fbac1bd70023c0cc5ba2c7f7cbe2ccd7050
KAVURUVEENACHOWDARY <74002021+KAVURUVEENACHOWDARY@users.noreply.github.com>
2021-05-14 23:01:16 +0530

    Add files via upload

---
+++ test_zipstream.py
@@ -0,0 +1,34 @@
+""" Tests for the TransferZipResponse. """
+import django.test
+
+from django_zip_stream.responses import TransferZipResponse
+
+
+class TransferZipResponseTestCase(django.test.TestCase):
+    """ Tests TransferZipResponse. """
+
+    def test_zip_single_file(self):
+        """ Test creating a zip for a single file. """
+        filename = __file__
+        files = [("/chicago.jpg", "/home/django/chicago.jpg", 4096), ]
+        response = TransferZipResponse(filename, files)
+        self.assertTrue(isinstance(response, TransferZipResponse))
+        self.assertEqual(response['X-Archive-Files'], 'zip')
+        self.assertEqual(response['Content-Type'], 'application/zip')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content,
+                         b"- 4096 /home/django/chicago.jpg /chicago.jpg")
+
+    def test_zip_multiple_files(self):
+        """ Test creating a zip for multiple files. """
+        filename = __file__
+        files = [("/chicago.jpg", "/home/django/chicago.jpg", 4096),
+                 ("/portland.jpg", "/home/django/portland.jpg", 4096), ]
+        response = TransferZipResponse(filename, files)
+        self.assertTrue(isinstance(response, TransferZipResponse))
+        self.assertEqual(response['X-Archive-Files'], 'zip')
+        self.assertEqual(response['Content-Type'], 'application/zip')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content,
+                         b"- 4096 /home/django/chicago.jpg /chicago.jpg\n"
+                         b"- 4096 /home/django/portland.jpg /portland.jpg")
